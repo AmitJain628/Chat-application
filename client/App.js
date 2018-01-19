1e0f1916af998fd134f3c686e346152735ce0e1f
AmitJain <amit.jain@limetray.com>
2018-01-19 12:31:11 +0530

    Added client folder

--- client/App.js
+++ client/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.welcome}>Open up App.js to start working on your app!</Text>
+        <Text style={styles.instructions}>Changes you make will automatically reload.</Text>
+        <Text style={styles.instructions}>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
